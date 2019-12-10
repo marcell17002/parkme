52fedc3f5532ca7b37fdcc3579c0ab6aa8f6ba05
marcell17002 <marcellantonius18@gmail.com>
2019-12-10 08:45:05 +0700

    new layout

--- app/src/main/java/com/example/last/ui/share/ShareFragment.kt
+++ app/src/main/java/com/example/last/ui/share/ShareFragment.kt
@@ -4,28 +4,49 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.example.last.R
+import com.google.android.gms.maps.GoogleMap
+import android.R
+import androidx.annotation.Nullable
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.LatLng
 
-class ShareFragment : Fragment() {
 
-    private lateinit var shareViewModel: ShareViewModel
+class ShareFragment : Fragment(), OnMapReadyCallback {
+
+    lateinit var map: GoogleMap
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        shareViewModel =
-            ViewModelProviders.of(this).get(ShareViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_share, container, false)
-//        val textView: TextView = root.findViewById(R.id.text_share)
-//        shareViewModel.text.observe(this, Observer {
-//            textView.text = it
-//        })
+        val root = inflater.inflate(com.example.last.R.layout.fragment_send, container, false)
         return root
+    }
+
+    override fun onViewCreated(view: View, @Nullable savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(com.example.last.R.id.map) as SupportMapFragment?
+        mapFragment!!.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        map.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+        val uiSettings = map.uiSettings
+        uiSettings.isZoomControlsEnabled = true
+
+        val pp = LatLng(13.8147954, -88.8636758)
+        map.addMarker(MarkerOptions().position(pp).title("Restaurante Ilobasco"))
+
+        val zoomlevel = 16f
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pp, zoomlevel))
     }
 }
